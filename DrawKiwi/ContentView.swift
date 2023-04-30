6fe6ac96a35c2eacceeaacf1417e104b6987ea17
YeeGuo <kiwi881217@gmail.com>
2023-04-30 14:59:54 +0800

    Initial Commit

---
+++ DrawKiwi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DrawKiwi
+//
+//  Created by 張凱棋 on 2023/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrawKiwi/DrawKiwiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawKiwiApp.swift
+//  DrawKiwi
+//
+//  Created by 張凱棋 on 2023/4/30.
+//
+
+import SwiftUI
+
+@main
+struct DrawKiwiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
